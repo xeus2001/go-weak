8cdea29438b94a08d7142d426fb6819e4343ec83
Alexander Lowey - Weber <alexander.weber@here.com>
2022-02-18 22:59:23 +0100

    Improved tests of Fnv1a to cover 100%.

--- pkg/weak/Fnv1a_test.go
+++ pkg/weak/Fnv1a_test.go
@@ -44,3 +44,19 @@ func TestFnv1a_abc(t *testing.T) {
 		t.Errorf("Expected 0xe71fa2190541574b, but found %x", fnv1a)
 	}
 }
+
+func TestFnv1a_nil(t *testing.T) {
+	h := weak.Fnv1a(nil, 10)
+	if h != 0xCBF29CE484222325 {
+		t.Errorf("Received wrong hash, expected 0xCBF29CE484222325, received: %x", h)
+	}
+}
+
+func TestFnv1a_negativeLength(t *testing.T) {
+	b := []byte{'a'}
+	array := weak.ToByteArray(&b)
+	h := weak.Fnv1a(array, -1)
+	if h != 0xCBF29CE484222325 {
+		t.Errorf("Received wrong hash, expected 0xCBF29CE484222325, received: %x", h)
+	}
+}
